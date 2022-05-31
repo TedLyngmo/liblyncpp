abda48d4c628fe3dbdb0fab14c356066225ce706
Ted Lyngmo <ted@lyncon.se>
2022-05-31 03:02:50 +0200

    Add timer_queue::synchronize()
    Signed-off-by: Ted Lyngmo <ted@lyncon.se>

--- include/lyn/timer_queue.hpp
+++ include/lyn/timer_queue.hpp
@@ -33,147 +33,168 @@ For more information, please refer to <https://unlicense.org>
 #include <chrono>
 #include <condition_variable>
 #include <functional>
+#include <future>
 #include <mutex>
 #include <queue>
+#include <type_traits>
 #include <utility>
-#include <vector>
 
 namespace lyn {
 namespace mq {
-template<class EventType = std::function<void()>,
-         class Clock = std::chrono::steady_clock,
-         class TimePoint = std::chrono::time_point<Clock>>
-class timer_queue {
-public:
-    using event_type = EventType;
-    using clock_type = Clock;
-    using duration = typename Clock::duration;
-    using time_point = TimePoint;
 
-private:
-    struct TimedEvent {
-        bool operator<(const TimedEvent& rhs) const { return rhs.StartTime < StartTime; }
-        time_point StartTime;
-        event_type m_event;
-    };
+    template<class EventType = std::function<void()>, class Clock = std::chrono::steady_clock,
+             class TimePoint = std::chrono::time_point<Clock>>
+    class timer_queue {
+    public:
+        using event_type = EventType;
+        using clock_type = Clock;
+        using duration = typename Clock::duration;
+        using time_point = TimePoint;
 
-public:
-    struct queue_type : std::priority_queue<TimedEvent> {
-        using std::priority_queue<TimedEvent>::pop;
+    private:
+        struct TimedEvent {
+            bool operator<(const TimedEvent& rhs) const { return rhs.StartTime < StartTime; }
+            time_point StartTime;
+            event_type m_event;
+        };
 
-        bool pop(event_type& ev) {
-            if(this->empty()) return false;
-            ev = std::move(this->top().m_event); // extract event
-            this->pop();
+    public:
+        struct queue_type : std::priority_queue<TimedEvent> {
+            using std::priority_queue<TimedEvent>::pop;
+
+            bool pop(event_type& ev) {
+                if(this->empty()) return false;
+                ev = std::move(this->top().m_event); // extract event
+                this->pop();
+                return true;
+            }
+        };
+
+        explicit timer_queue(duration now_delay) : m_now_delay(std::move(now_delay)) {}
+
+        timer_queue() : timer_queue(std::chrono::nanoseconds(0)) {}
+        timer_queue(const timer_queue&) = delete;
+        timer_queue(timer_queue&&) = delete;
+        timer_queue& operator=(const timer_queue&) = delete;
+        timer_queue& operator=(timer_queue&&) = delete;
+        ~timer_queue() { shutdown(); }
+
+        void shutdown() {
+            m_shutdown = true;
+            m_cv.notify_all();
+        }
+        void clear() {
+            std::lock_guard<std::mutex> lock(m_mutex);
+            m_queue = queue_type{};
+        }
+        void restart() { m_shutdown = false; }
+
+        bool is_open() const { return !m_shutdown; }
+        bool operator!() const { return m_shutdown; }
+        explicit operator bool() const { return !m_shutdown; }
+
+        // synchronize requires EventType to return `void` and be capable of captures.
+        template<class R, class Func>
+        R synchronize(Func&& func) {
+            std::promise<R> p;
+            std::future<R> f = p.get_future();
+
+            if constexpr(std::is_same_v<void, R>) {
+                emplace_do_urgently([&p, func = std::forward<Func>(func)](auto&&... args) {
+                    func(std::forward<decltype(args)>(args)...);
+                    p.set_value();
+                });
+
+                f.wait();
+            } else {
+                emplace_do_urgently([&p, func = std::forward<Func>(func)](auto&&... args) {
+                    p.set_value(func(std::forward<decltype(args)>(args)...));
+                });
+
+                f.wait();
+                return f.get();
+            }
+        }
+
+        template<class... Args>
+        void emplace_do_in(duration dur, Args&&... args) {
+            std::lock_guard<std::mutex> lock(m_mutex);
+
+            m_queue.emplace(TimedEvent{clock_type::now() + dur, std::forward<Args>(args)...});
+            m_cv.notify_all();
+        }
+
+        template<class TP, class... Args>
+        void emplace_do_at(TP&& tp, Args&&... args) {
+            std::lock_guard<std::mutex> lock(m_mutex);
+
+            m_queue.emplace(TimedEvent{std::forward<TP>(tp), std::forward<Args>(args)...});
+            m_seq += std::chrono::nanoseconds(1);
+            m_cv.notify_all();
+        }
+
+        template<class... Args>
+        void emplace_do(Args&&... args) {
+            emplace_do_in(m_now_delay, std::forward<Args>(args)...);
+        }
+
+        template<class... Args>
+        void emplace_do_urgently(Args&&... args) {
+            emplace_do_at(m_seq, std::forward<Args>(args)...);
+        }
+
+        bool wait_pop(event_type& ev) {
+            std::unique_lock<std::mutex> lock(m_mutex);
+
+            while(not m_shutdown && (m_queue.empty() || clock_type::now() < m_queue.top().StartTime)) {
+                if(m_queue.empty()) {
+                    m_cv.wait(lock);
+                } else {
+                    auto st = m_queue.top().StartTime;
+                    m_cv.wait_until(lock, st);
+                }
+            }
+            if(m_shutdown) return false; // time to quit
+
+            ev = std::move(m_queue.top().m_event); // extract event
+            m_queue.pop();
+
             return true;
         }
+
+        bool wait_pop_all(queue_type& in_out) {
+            in_out = queue_type{};
+            std::unique_lock<std::mutex> lock(m_mutex);
+
+            while(not m_shutdown && (m_queue.empty() || clock_type::now() < m_queue.top().StartTime)) {
+                if(m_queue.empty()) {
+                    m_cv.wait(lock);
+                } else {
+                    auto st = m_queue.top().StartTime;
+                    m_cv.wait_until(lock, st);
+                }
+            }
+            if(m_shutdown) return false;
+
+            auto now = clock_type::now();
+            while(!m_queue.empty() && now >= m_queue.top().StartTime) {
+                in_out.emplace(std::move(m_queue.top()));
+                m_queue.pop();
+            }
+
+            return true;
+        }
+
+    private:
+        queue_type m_queue{};
+        mutable std::mutex m_mutex{};
+        std::condition_variable m_cv{};
+        std::atomic<bool> m_shutdown{};
+        duration m_now_delay;
+        // m_seq is used for to make sure events are executed in the order they are put in the queue which can be used
+        // to extend the queue with adding a bunch of elements at the same time and to guarantee them to be extracted
+        // together in the exact order they were put in.
+        time_point m_seq{};
     };
-
-    explicit timer_queue(duration now_delay) : m_now_delay(std::move(now_delay)) {}
-
-    timer_queue() : timer_queue(std::chrono::nanoseconds(0)) {}
-    timer_queue(const timer_queue&) = delete;
-    timer_queue(timer_queue&&) = delete;
-    timer_queue& operator=(const timer_queue&) = delete;
-    timer_queue& operator=(timer_queue&&) = delete;
-    ~timer_queue() { shutdown(); }
-
-    void shutdown() {
-        m_shutdown = true;
-        m_cv.notify_all();
-    }
-    void clear() {
-        std::lock_guard<std::mutex> lock(m_mutex);
-        m_queue = queue_type{};
-    }
-    void restart() {
-        m_shutdown = false;
-    }
-
-    bool is_open() const { return !m_shutdown; }
-    bool operator!() const { return m_shutdown; }
-    explicit operator bool() const { return !m_shutdown; }
-
-    template<class... Args>
-    void emplace_do_in(duration dur, Args&&... args) {
-        std::lock_guard<std::mutex> lock(m_mutex);
-
-        m_queue.emplace(TimedEvent{clock_type::now() + dur, std::forward<Args>(args)...});
-        m_cv.notify_all();
-    }
-
-    template<class TP, class... Args>
-    void emplace_do_at(TP&& tp, Args&&... args) {
-        std::lock_guard<std::mutex> lock(m_mutex);
-
-        m_queue.emplace(TimedEvent{std::forward<TP>(tp), std::forward<Args>(args)...});
-        m_seq += std::chrono::nanoseconds(1);
-        m_cv.notify_all();
-    }
-
-    template<class... Args>
-    void emplace_do(Args&&... args) {
-        emplace_do_in(m_now_delay, std::forward<Args>(args)...);
-    }
-
-    template<class... Args>
-    void emplace_do_urgently(Args&&... args) {
-        emplace_do_at(m_seq, std::forward<Args>(args)...);
-    }
-
-    bool wait_pop(event_type& ev) {
-        std::unique_lock<std::mutex> lock(m_mutex);
-
-        while(not m_shutdown && (m_queue.empty() || clock_type::now() < m_queue.top().StartTime)) {
-            if(m_queue.empty()) {
-                m_cv.wait(lock);
-            } else {
-                auto st = m_queue.top().StartTime;
-                m_cv.wait_until(lock, st);
-            }
-        }
-        if(m_shutdown) return false; // time to quit
-
-        ev = std::move(m_queue.top().m_event); // extract event
-        m_queue.pop();
-
-        return true;
-    }
-
-    bool wait_pop_all(queue_type& in_out) {
-        in_out = queue_type{};
-        std::unique_lock<std::mutex> lock(m_mutex);
-
-        while(not m_shutdown && (m_queue.empty() || clock_type::now() < m_queue.top().StartTime)) {
-            if(m_queue.empty()) {
-                m_cv.wait(lock);
-            } else {
-                auto st = m_queue.top().StartTime;
-                m_cv.wait_until(lock, st);
-            }
-        }
-        if(m_shutdown) return false;
-
-        auto now = clock_type::now();
-        while(!m_queue.empty() && now >= m_queue.top().StartTime) {
-            in_out.emplace(std::move(m_queue.top()));
-            m_queue.pop();
-        }
-
-        return true;
-    }
-
-
-private:
-    queue_type m_queue{};
-    mutable std::mutex m_mutex{};
-    std::condition_variable m_cv{};
-    std::atomic<bool> m_shutdown{};
-    duration m_now_delay;
-    // m_seq is used for to make sure events are executed in the order they are put in the queue which can be used to
-    // extend the queue with adding a bunch of elements at the same time and to guarantee them to be extracted together
-    // in the exact order they were put in.
-    time_point m_seq{};
-};
 } // namespace mq
 } // namespace lyn
--- mq/example_timer_queue.cpp
+++ mq/example_timer_queue.cpp
@@ -3,42 +3,44 @@
 #include <atomic>
 #include <iostream>
 #include <thread>
-using queue_t = lyn::mq::timer_queue<>;
 
-void one(queue_t& q) {
+using queue_1_t = lyn::mq::timer_queue<>;
+
+void one(queue_1_t& q) {
     for(int i = 0; q; ++i) {
         std::this_thread::sleep_for(std::chrono::seconds(1));
         q.emplace_do([i] { std::cout << "one: " << i << '\n'; });
     }
 }
 
-void two(queue_t& q) {
+void two(queue_1_t& q) {
     for(int i = 0; q; ++i) {
         std::this_thread::sleep_for(std::chrono::seconds(2));
         q.emplace_do([i] { std::cout << "two: " << i << '\n'; });
     }
 }
 
-void three(queue_t& q) {
+void three(queue_1_t& q) {
     for(int i = 0; q; ++i) {
         std::this_thread::sleep_for(std::chrono::milliseconds(10));
         q.emplace_do([i] { std::cout << "three: " << i << '\n'; });
     }
 }
 
-int main() {
-    queue_t q(std::chrono::seconds(1));
+void test1() {
+    queue_1_t q(std::chrono::seconds(1));
+
     auto twoth = std::thread(&two, std::ref(q));
     auto oneth = std::thread(&one, std::ref(q));
 
-    q.emplace_do_at(queue_t::clock_type::now() + std::chrono::seconds(10),
+    q.emplace_do_at(queue_1_t::clock_type::now() + std::chrono::seconds(10),
             [&q] { std::cout << "shutdown\n"; q.shutdown(); });
 
-    q.emplace_do_at(queue_t::clock_type::now() + std::chrono::milliseconds(4500),
+    q.emplace_do_at(queue_1_t::clock_type::now() + std::chrono::milliseconds(4500),
             [] { std::cout << "Hello world\n"; });
 
 
-    queue_t::event_type ev;
+    queue_1_t::event_type ev;
     while(q.wait_pop(ev)) {
         ev();
     }
@@ -57,4 +59,29 @@ int main() {
         }
     }
     threeth.join();
+}
+// -----
+using queue_2_t = lyn::mq::timer_queue<std::function<void(int,double)>>;
+
+void sync(queue_2_t& q) {
+    auto res = q.synchronize<double>([](int i, double d) -> double {
+        return i + d;
+    });
+    q.shutdown();
+    std::cout << "got " << res << " via synchronize\n";
+}
+
+void test2() {
+    queue_2_t q;
+    auto syncth = std::thread(&sync, std::ref(q));
+    queue_2_t::event_type ev;
+    while(q.wait_pop(ev)) {
+        ev(1, 3.14159);
+    }
+    syncth.join();
+}
+// -----
+int main() {
+    test1();
+    test2();
 }
